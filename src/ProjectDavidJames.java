abed0e9c464b501d4726e7be5439266af9d3c6e1
SlimeDev18 <SlimeDev18@gmail.com>
2026-03-23 18:17:40 -0400

    Added code to display all policy information the number of smokers, and the number of non-smokers

--- src/ProjectDavidJames.java
+++ src/ProjectDavidJames.java
@@ -16,6 +16,7 @@ public class ProjectDavidJames {
     String smoker;
     double height;
     double weight;
+    int totalSmokers = 0;
     try (Scanner fileReader = new Scanner(policyFile)) {
       while (fileReader.hasNextLine()) {
         String line = fileReader.nextLine().trim();
@@ -34,19 +35,8 @@ public class ProjectDavidJames {
           weight = Double.parseDouble(fileReader.nextLine().trim());
 
           Policy insurance = new Policy(policyNum, providerName, fName, lName, age, smoker, height, weight);
+          policies.add(insurance);
 
-          System.out.println("\nPolicy Information");
-          System.out.println("----------------------------");
-          System.out.println("Policy Number: " + insurance.getPolicyNum());
-          System.out.println("Provider Name: " + insurance.getProviderName());
-          System.out.println("Policyholder's First Name: " + insurance.getFName());
-          System.out.println("Policyholder's Last Name: " + insurance.getLName());
-          System.out.println("Policyholder's Age: " + insurance.getAge());
-          System.out.println("Policyholder's Smoking Status: " + insurance.getSmoker());
-          System.out.println("Policyholder's Height: " + insurance.getHeight() + " inches");
-          System.out.println("Policyholder's Weight: " + insurance.getWeight() + " pounds");
-          System.out.printf("Policyholder's BMI: %.2f\n", insurance.calcBMI());
-          System.out.printf("Policy Price: $%.2f\n", insurance.calcInsurance());
         } catch (NumberFormatException | NoSuchElementException e) {
           System.out.println("ERROR: Invalid data format in file. Skipping entry.");
         }
@@ -54,6 +44,30 @@ public class ProjectDavidJames {
     } catch (FileNotFoundException e) {
       System.out.println("ERROR: File 'PolicyInformation.txt' not found");
     }
+
+    for (int i = 0; i < policies.size(); i++) {
+
+      if (policies.get(i).getSmoker().equalsIgnoreCase("smoker")) {
+        totalSmokers += 1;
+      }
+
+      System.out.printf("---------------------------------------\n");
+      System.out.printf("Policy Information:\n");
+      System.out.printf("Policy Number: %d\n", policies.get(i).getPolicyNum());
+      System.out.printf("Provider Name: %s\n", policies.get(i).getProviderName());
+      System.out.printf("Policyholder's First Name: %s\n", policies.get(i).getFName());
+      System.out.printf("Policyholder's Last Name: %s\n", policies.get(i).getLName());
+      System.out.printf("Policyholder's Age: %d\n", policies.get(i).getAge());
+      System.out.printf("Policyholder's Smoking Status (smoker/non-smoker): %s\n", policies.get(i).getSmoker());
+      System.out.printf("Policyholder's Height: %.2f inches\n", policies.get(i).getHeight());
+      System.out.printf("Policyholder's Weight: %.2f pounds\n", policies.get(i).getWeight());
+      System.out.printf("Policyholder's BMI: %.2f\n", policies.get(i).calcBMI());
+      System.out.printf("Policy Price: $%.2f\n", policies.get(i).calcInsurance());
+      System.out.printf("---------------------------------------\n\n");
+    }
+
+    System.out.printf("The number of policies with a smoker is: %d\n", totalSmokers);
+    System.out.printf("The number of policies with a non-smoker is: %d\n", policies.size() - totalSmokers);
 
   }
 }
